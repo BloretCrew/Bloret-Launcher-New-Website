5abd91e1f3d0c08ee851da42a69a1a78d7035959
Detritalw <Detritalw@outlook.com>
2025-11-02 08:26:08 +0800

    ui web demo

--- ui-web-demo/vite.config.ts
+++ ui-web-demo/vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  base: '/Bloret-Launcher-New-Website/',
   server: {
     port: 5173
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 })
